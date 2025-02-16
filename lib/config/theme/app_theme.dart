e77d22c07d7965228fc1a79add2b23d87df3be22
Abraham Herrera <abrahamhs.mlm@gmail.com>
2025-02-16 20:57:00 +0100

    Add theme configuration and apply it to the main application

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,34 @@
+
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.red,
+  Colors.green,
+  Colors.blue,
+  Colors.yellow,
+  Colors.orange,
+  Colors.purple,
+  Colors.pink,
+  Colors.teal,
+  Colors.cyan,
+  Colors.indigo,
+  Colors.lime,
+  Colors.amber,
+  Colors.brown,
+  Colors.grey,
+    
+];
+
+class AppTheme {
+  final int selectedColorIndex;
+
+   AppTheme({
+    this.selectedColorIndex = 0
+    }):assert(selectedColorIndex >= 0, 'selectedColorIndex must be greater than 0'),
+    assert(selectedColorIndex < colorList.length, 'selectedColorIndex must be less than ${colorList.length}');
+    ThemeData getTheme() => ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: colorList[selectedColorIndex],
+    );
+  
+}

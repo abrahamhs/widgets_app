6cadbddd0d06920967122c39f1e283fd0cc4ff8a
Abraham Herrera <abrahamhs.mlm@gmail.com>
2025-02-17 06:53:37 +0100

    Add navigation routes for ButtonsScreen and CardsScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/presentation/screen/buttons/buttons_screen.dart';
+import 'package:widgets_app/presentation/screen/cards/cards_screen.dart';
 
 import 'config/theme/app_theme.dart';
 import 'presentation/screen/home/home_screen.dart';
@@ -16,6 +18,10 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColorIndex: 5).getTheme(),
       home: const HomeScreen(),  
+      routes: {
+        '/buttons': (context) => const ButtonsScreen(),
+        '/cards': (context) => const CardsScreen(),
+      },
       
     );
   }
--- lib/presentation/screen/home/home_screen.dart
+++ lib/presentation/screen/home/home_screen.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 
+import '../buttons/buttons_screen.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter + Material 3'),
-      ),
-      body:_HomeView()
-    );
+        appBar: AppBar(
+          title: const Text('Flutter + Material 3'),
+        ),
+        body: _HomeView());
   }
 }
 
@@ -20,16 +21,12 @@ class _HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
-      
     return ListView.builder(
-      itemCount: appMenuItems.length,
-      itemBuilder: (context, index) {
-        final menuItem = appMenuItems[index];
-        return _CustomListTitle(menuItem: menuItem);
-          
-      }
-    );
+        itemCount: appMenuItems.length,
+        itemBuilder: (context, index) {
+          final menuItem = appMenuItems[index];
+          return _CustomListTitle(menuItem: menuItem);
+        });
   }
 }
 
@@ -42,18 +39,20 @@ class _CustomListTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).colorScheme;                                     
+    final colors = Theme.of(context).colorScheme;
     return ListTile(
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
       leading: Icon(menuItem.icon, color: colors.primary),
-      trailing:  Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
       onTap: () {
-        // TODO: Navegar a otra pantalla
-        
-        // Navigator.pushNamed(context, menuItem.link);
+         Navigator.pushNamed(context, menuItem.link);
+        /* Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => ButtonsScreen(),
+          ),
+        ); */
       },
-      
     );
   }
 }

52549a0747e3a32c18dee37e834975504428f7aa
Abraham Herrera <abrahamhs.mlm@gmail.com>
2025-02-18 21:36:03 +0100

    Add new screens for animated, app tutorial, infinite scroll, progress, snackbar, and UI controls

---
+++ lib/presentation/screen/infinite_scroll/infinite_scroll_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class InfiniteScroollScreen extends StatelessWidget {
+  static const String routeName = 'infinite_scroll_screen';
+  const InfiniteScroollScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
